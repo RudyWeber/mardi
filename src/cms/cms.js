5fcbe42c3728da5a46878d0bd5491b1d7e121792
Cati Mayer <cati.mayer123@gmail.com>
2020-10-18 15:29:03 +0200

    cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import WhoWeHelpPagePreview from "./preview-templates/WhoWeHelpPagePreview";
 import VolunteerPagePreview from "./preview-templates/VolunteerPagePreview";
+import RefugeeCrisisPagePreview from "./preview-templates/RefugeeCrisisPagePreview";
 import FaqPagePreview from "./preview-templates/FaqPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
@@ -13,6 +14,7 @@ CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("who-we-help", WhoWeHelpPagePreview);
+CMS.registerPreviewTemplate("refugee-crisis", RefugeeCrisisPagePreview);
 CMS.registerPreviewTemplate("volunteer", VolunteerPagePreview);
 CMS.registerPreviewTemplate("faq", FaqPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
