2c6198e9364255e1c31c5fff628a318084518a0a
Rudy Weber <webrudy@gmail.com>
2020-11-01 14:12:40 +0100

    open donate page in a new tab

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -64,7 +64,7 @@ export const IndexPageTemplate = ({
         <form
           action="https://www.paypal.com/cgi-bin/webscr"
           method="post"
-          target="_top"
+          target="_blank"
         >
           <input name="cmd" type="hidden" value="_s-xclick" />
           <input name="hosted_button_id" type="hidden" value="7CT2YW5N47BKU" />
