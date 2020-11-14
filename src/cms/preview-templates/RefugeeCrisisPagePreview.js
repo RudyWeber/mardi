74153697da0b5ceb7ca3d1870c06a48424908161
Rudy Weber <webrudy@gmail.com>
2020-11-14 19:05:37 +0100

    urrgh

--- src/cms/preview-templates/RefugeeCrisisPagePreview.js
+++ src/cms/preview-templates/RefugeeCrisisPagePreview.js
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 import { RefugeeCrisisPageTemplate } from "../../templates/refugee-crisis-page";
 import { HTMLContent } from "../../components/Content";
 
-const RefugeeCrisisPagePreview = ({ entry }) => {
-  const data = entry.getIn(["data"]);
+const RefugeeCrisisPagePreview = (props) => {
+  console.log(Object.entries(props).map(([k, v]) => [k, v.toJS ? v.toJS() : v]));
+  const data = props.entry.getIn(["data"]);
   const { title, body, slideshowImages } = data.toJS ? data.toJS() : data;
 
   return (
