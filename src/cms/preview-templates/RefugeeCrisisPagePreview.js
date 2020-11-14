7da0c4cf9aa9962bfc997adad561b5823277e712
Rudy Weber <webrudy@gmail.com>
2020-11-14 18:34:15 +0100

    try stuff.

--- src/cms/preview-templates/RefugeeCrisisPagePreview.js
+++ src/cms/preview-templates/RefugeeCrisisPagePreview.js
@@ -2,13 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import { RefugeeCrisisPageTemplate } from "../../templates/refugee-crisis-page";
 
-const RefugeeCrisisPagePreview = ({ entry }) => (
+const RefugeeCrisisPagePreview = ({ entry }) => {
+  const data = entry.getIn(["data"]);
+  const dataJs = data.toJS ? data.toJS() : data;
+  const content = entry.getIn(["data", "content"]);
+
+  console.log(data);
+  console.log(dataJs);
+  console.log(content);
+
+  return (
   <RefugeeCrisisPageTemplate
     title={entry.getIn(["data", "title"])}
     content={entry.getIn(["data", "content"])}
     slideshowImages={entry.getIn(["data", "slideshowImages"]).toJS()}
   />
-);
+)};
 
 RefugeeCrisisPagePreview.propTypes = {
   entry: PropTypes.shape({
