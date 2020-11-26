5fc36ab648fa69854dab7919065ff585ca5b5241
Rudy Weber <webrudy@gmail.com>
2020-11-26 17:50:10 +0100

    logo review

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.png";
+import logo from "../img/logo.jpeg";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 
