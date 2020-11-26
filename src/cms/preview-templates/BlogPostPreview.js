2e912bb226dad06a6decbec93f6675fd59e1e385
Rudy Weber <webrudy@gmail.com>
2020-11-26 17:48:51 +0100

    remove tags

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types'
 import { CustomBlogPostTemplate } from './customPreviews/CustomBlogPostTemplate'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
   return (
     <CustomBlogPostTemplate
       content={widgetFor('body')}
       description={entry.getIn(['data', 'description'])}
-      tags={tags && tags.toJS()}
       title={entry.getIn(['data', 'title'])}
     />
   )
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -72,7 +72,6 @@ const BlogPost = ({ data }) => {
             />
           </Helmet>
         }
-        tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
@@ -95,9 +94,7 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        description
-        tags
       }
     }
   }
-`
+`;
